bd9b8ee5c606ae7a440bc2fe404b802c179812c5
Adrian Azoitei <azo.adrian@gmail.com>
2023-05-13 09:34:28 +0200

    feat: initial commit

---
+++ src/content/config.ts
@@ -0,0 +1,29 @@
+import { z, defineCollection } from "astro:content";
+
+const blogSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.string().optional(),
+    heroImage: z.string().optional(),
+    badge: z.string().optional(),
+});
+
+const projectSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    rank: z.number(),
+    badge: z.string().optional(),
+    heroImage: z.string().optional(),
+});
+
+export type BlogSchema = z.infer<typeof blogSchema>;
+export type ProjectSchema = z.infer<typeof projectSchema>;
+
+const blogCollection = defineCollection({ schema: blogSchema });
+const projectCollection = defineCollection({ schema: projectSchema });
+
+export const collections = {
+    'blog': blogCollection,
+    'projects': projectCollection
+}
