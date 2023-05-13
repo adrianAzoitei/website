ed7a5657f0cfeb03c95b7c28d3d452cebb0f6701
Adrian Azoitei <azo.adrian@gmail.com>
2023-05-13 09:37:34 +0200

    feat: config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-modern-personal-website.netlify.app',
+  site: 'https://adrianazoitei.github.io',
+  base: '/website',
   integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
