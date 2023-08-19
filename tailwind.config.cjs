ed6014a855f726fc4553d52df620f202727cba2f
Adrian Azoitei <azo.adrian@gmail.com>
2023-08-19 13:50:40 +0200

    feat: first positioning of swiss army knife

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			spacing: {
+			 '88': '22rem'
+			}
+		},
+		height: {
+			'100': '30rem',
+		   },
+		rotate: {
+			 '-90': '-90deg',
+			 '0': '0',
+			 '45': '45deg',
+			 '90': '90deg',
+			'30': '30deg',
+			 '180': '180deg',
+			'270': '270deg',
+		   }
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 }
