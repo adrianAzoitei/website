9e2d3821e54c6d68b1ae16d1f6e72dc2ecd3b131
Adrian Azoitei <azo.adrian@gmail.com>
2023-11-05 15:14:52 +0100

    fix: add base

--- astro.config.ts
+++ astro.config.ts
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://adrianazoitei.github.io',
+  base: 'website',
   integrations: [mdx(), sitemap(), tailwind()]
 });
