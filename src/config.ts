cc996338b547a6b1850a9affec917667e162c38f
Adrian Azoitei <azo.adrian@gmail.com>
2024-02-06 20:32:38 +0100

    chore: update title and description

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'gharrus.dev';
-export const SITE_DESCRIPTION = 'Personal website of Adrian Azoitei.';
+export const SITE_TITLE = 'pryingbee.com';
+export const SITE_DESCRIPTION = 'Personal website showcasing what I can do with software.';
